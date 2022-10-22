063766290460e64c1657fb44f0882733cfa15161
ilax666 <thierryverne41@gmail.com>
2022-10-22 19:21:17 +0200

    test

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
 void setup() {
   // put your setup code here, to run once:
-
+  //e
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
 }
-=======
-
->>>>>>> 3439e1780092801f0633d2fe4ccba834d270be56
